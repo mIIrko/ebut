f4ef9fb94d1f900e5945ee04a12d3a98746348bc
miirko <mirkoo@posteo.org>
2017-06-17 19:01:37 +0200

    added file and resources

---
+++ files/ebus_articlenumbertype.sql
@@ -0,0 +1,3 @@
+INSERT INTO ebus.articlenumbertype (articlenumbertype, description) VALUES ('EAN', 'Europäische Artikelnummer');
+INSERT INTO ebus.articlenumbertype (articlenumbertype, description) VALUES ('HerstNr', 'Herstellerspezifisches Artikelnummer');
+INSERT INTO ebus.articlenumbertype (articlenumbertype, description) VALUES ('UPC', 'Universal Product Code');
